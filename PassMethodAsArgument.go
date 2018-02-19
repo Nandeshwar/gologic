20584a96b371184d0b844ba47c3a4c0f2c25c8df
Nandeshwar  Sah <nandeshwarsah@Nandeshwars-iMac.local>
2018-02-18 18:52:31 -0700

    Passed anonymous function defintion in function

--- PassMethodAsArgument.go
+++ PassMethodAsArgument.go
@@ -3,13 +3,25 @@ package main
 import "fmt"
 
 type AnyFun func (a int, b int) int
+
 func main(){
+	// Passing named method definition in function
 	myFun(add)
 	myFun(sub)
+	myFun2(add)
+
+	// Passing anonymous method definition in function
+	myFun2(func(a int, b int) int{
+		return a * b
+	})
 }
 
 func myFun(f AnyFun){
 	fmt.Println(f(20, 10))
+}
+
+func myFun2(f func(a int, b int) int){
+	fmt.Println(f(10, 20))
 }
 
 func add(a int, b int) int {
