15dd75d7a369e12124f00d9b3e95bced23361841
Nandeshwa Sah <nandeshwar.sah@dish.com>
2018-03-06 15:48:01 -0700

    Added program to insert item

--- linked-list-example.go
+++ linked-list-example.go
@@ -5,29 +5,34 @@ import "fmt"
 type Node struct {
 	item int
 	next *Node
+	start *Node
 }
 
 var (
-	start *Node
 	current *Node
 	isFirst = true
 )
 
 func main() {
-	addItem(10)
-	addItem(20)
-	addItem(30)
-	addItem(40)
-	display()
+	node := &Node{}
+	node.addItem(10)
+	node.addItem(20)
+	node.addItem(30)
+	node.addItem(40)
+	if node.insertItem(30, 50) {
+		fmt.Println("Item inserted successfully")
+	} else {
+		fmt.Println("Middle item not found insertion failed")
+	}
+	node.display()
 }
 
-func addItem(item int){
+func (n *Node) addItem(item int){
 	if isFirst {
-		start = &Node{
-			item: item,
-			next: nil,
-		}
-		current = start
+		n.item = item
+		n.next = nil
+		n.start = n
+		current = n.start
 		isFirst = false
 	} else {
 		current.next = &Node {
@@ -38,8 +43,26 @@ func addItem(item int){
 	}
 }
 
-func display() {
-	for begin := start; ; begin = begin.next {
+func (n *Node) insertItem(after int, item int) (isFound bool){
+	for begin := n.start; ; begin = begin.next {
+
+		if begin.item == after {
+			isFound = true
+			begin.next = &Node {
+				item: item,
+				next: begin.next,
+			}
+			return true
+		}
+		if begin.next == nil {
+			break
+		}
+	}
+	return
+}
+
+func (n *Node) display() {
+	for begin := n.start; ; begin = begin.next {
 		fmt.Println(begin.item)
 		if begin.next == nil {
 			break
