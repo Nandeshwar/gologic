4ac081843f3c2ef1bd3e20b416f6b9937348c90d
Nandeshwar <nandeshwar.sah@gmail.com>
2019-07-15 19:41:28 -0600

    Create write_read_binary.go

---
+++ write_read_binary.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+func main() {
+
+	type data struct {
+		Val1 uint8
+		Val2 uint8
+	}
+
+	d := &data{}
+	s := &data{
+		Val1: uint8(0x48),
+		Val2: uint8(0x28),
+	}
+
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, s)
+	if err != nil {
+		fmt.Println("error ....=", err.Error())
+	}
+
+	reader := bytes.NewReader(buf.Bytes())
+
+	if err := binary.Read(reader, binary.LittleEndian, d); err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+
+	fmt.Println(d.Val1 & 0xF)
+	fmt.Println(d.Val2 >> 5)
+}
