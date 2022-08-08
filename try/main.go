792478ed1725cc83b7399137f8feecff47d86acc
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-08-07 19:50:40 -0600

    added a example

---
+++ try/main.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	x := ""
+	data := Employee{
+        FirstName: "Mark",
+        LastName:  "Jones",
+        Email:     "mark@gmail.com",
+        Age:       25,
+        MonthlySalary: []Salary{
+            Salary{
+                Basic: 15000.00,
+                HRA:   5000.00,
+                TA:    2000.00,
+            },
+            
+            []Salary{
+                Basic: 16000.00,
+                HRA:   5000.00,
+                TA:    2100.00,
+            },
+          
+            Salary{
+                Basic: 17000.00,
+                HRA:   5000.00,
+                TA:    2200.00,
+            },
+        },
+    }
+ 	
+	fmt.Println(data)
+	file, _ := json.MarshalIndent(data, "", " ")
+ 
+	_ = ioutil.WriteFile("test.json", file, 0644)
+}
