1d6594db1928ca3abd791581eb0e390274af3151
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-08-27 14:04:56 -0600

    updated example

--- try2/example18_tower_of_henoi/main.go
+++ try2/example18_tower_of_henoi/main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	towerOfHenoi(3, "t1", "t3", "t2") // move from t1 to t3 using t2
+	fmt.Println("Another way")
+	th(3, "t1", "t3", "t2")
 }
 
 /*
@@ -33,3 +35,12 @@ move disk=1 from t2 to t1
 move disk=2 from t2 to t3
 move disk=1 from t1 to t3
 */
+
+func th(n int, source, dest, tmp string) {
+	if n == 0 {
+		return
+	}
+	th(n-1, source, tmp, dest)
+	fmt.Printf("\n move disk=%d from %s to %s", n, source, dest)
+	th(n-1, tmp, dest, source)
+}
