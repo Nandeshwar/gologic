d3018100a8fb450e6d335e939893b0ca1f51a056
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-09-05 13:28:13 -0600

    updated examples

--- try2/example34_max_in_array_recursion/main.go
+++ try2/example34_max_in_array_recursion/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println(max([]int{1, 20, 10, 5, 9}, 0))
+	fmt.Println(max2([]int{1, 20, 10, 5, 9}, 0))
 }
 
 func max(a []int, ind int) int {
@@ -16,4 +17,16 @@ func max(a []int, ind int) int {
 		return m
 	}
 	return a[ind]
+}
+
+func max2(a []int, ind int) int {
+	if len(a) == 1 {
+		return a[0]
+	}
+
+	m := max2(a[1:], ind+1)
+	if m > a[0] {
+		return m
+	}
+	return a[0]
 }
