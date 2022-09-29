78c527021f0bb81edfa2674fb7ede59ac6fe2eee
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-09-28 19:37:46 -0600

    added example

---
+++ try2/example60_stairs_path/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	allPaths := stairsAllPath(4)
+	fmt.Println(allPaths)
+}
+
+// can jump 1 or 2 or 3
+func stairsAllPath(n int) []string {
+	if n == 0 {
+		return []string{""}
+	}
+	if n < 0 {
+		return []string{}
+	}
+
+	paths1 := stairsAllPath(n - 1)
+	paths2 := stairsAllPath(n - 2)
+	paths3 := stairsAllPath(n - 3)
+
+	var paths []string
+
+	for _, v := range paths1 {
+		paths = append(paths, "1"+v)
+	}
+
+	for _, v := range paths2 {
+		paths = append(paths, "2"+v)
+	}
+
+	for _, v := range paths3 {
+		paths = append(paths, "3"+v)
+	}
+	return paths
+}
