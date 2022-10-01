574e018a2575a6e11887ea5912b6036f76fe5807
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-09-30 18:34:09 -0600

    Added example

---
+++ try2/example65_selection_sort/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := []int{1, 20, 2, 30, 3, 7, 5}
+	selectionSort(a)
+	fmt.Println(a)
+}
+
+func selectionSort(a []int) {
+	for i := 0; i < len(a); i++ {
+		min := i
+		for j := i; j < len(a); j++ {
+			if a[j] < a[min] {
+				min = j
+			}
+		}
+		if i != min {
+			a[i], a[min] = a[min], a[i]
+		}
+	}
+}
