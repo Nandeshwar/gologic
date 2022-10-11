1a348a7a34125093263d1d84a3bf874a233c0653
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-10-10 20:22:59 -0600

    Added example

---
+++ try2/example81_find_celebrety/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// everyone know celebrety. Celebrety does not know anyone
+	fmt.Println(findCelebrety(5))
+}
+
+func findCelebrety(n int) int {
+	candidate := 0
+	for i := 1; i < n; i++ {
+		if know(candidate, i) {
+			candidate = i
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if i != candidate && (know(candidate, i) || !know(i, candidate)) {
+			return -1
+		}
+	}
+	return candidate
+}
+
+func know(candidate, i int) bool {
+	if candidate == 3 {
+		return false
+	}
+	return true
+}
