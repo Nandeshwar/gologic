991df86c4f5a1e43b2b158939c20dfc65fe190fa
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-10-15 21:37:55 -0600

    updated example

--- try2/example78_search_infinite_sorted_arr/main.go
+++ try2/example78_search_infinite_sorted_arr/main.go
@@ -8,12 +8,12 @@ func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
 
 	beg := 0
-	end := 2
+	end := 1
 
 	search := 11
 
 	for end < search {
-		end = end * end
+		end = end * 2
 	}
 
 	fmt.Println("end=", end)
