d242732066d06ed65af8777b4053feb29a0eb40c
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-11-05 11:10:18 -0600

    updated logic

--- try2/example65_selection_sort/main.go
+++ try2/example65_selection_sort/main.go
@@ -13,7 +13,7 @@ func main() {
 func selectionSort(a []int) {
 	for i := 0; i < len(a); i++ {
 		min := i
-		for j := i; j < len(a); j++ {
+		for j := i + 1; j < len(a); j++ {
 			if a[j] < a[min] {
 				min = j
 			}
