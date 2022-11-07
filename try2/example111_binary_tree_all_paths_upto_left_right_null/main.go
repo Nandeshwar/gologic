831fa26e5a968d76ceed079fe2171297c348fdd7
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-11-06 17:01:25 -0700

    added example

---
+++ try2/example111_binary_tree_all_paths_upto_left_right_null/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Tree struct {
+	item  int
+	left  *Tree
+	right *Tree
+}
+
+/*
+		10
+	  7   9
+	5  6
+
+
+*/
+func main() {
+	five := Tree{item: 5}
+	six := Tree{item: 6}
+	t := &Tree{item: 10, left: &Tree{item: 7, left: &five, right: &six}, right: &Tree{item: 9}}
+
+	paths := allPaths(t, []int{})
+	fmt.Println(paths)
+
+}
+
+func allPaths(t *Tree, paths []int) [][]int {
+	paths = append(paths, t.item)
+
+	if t.left == nil && t.right == nil {
+		fmt.Println("paths=", paths)
+		return [][]int{paths}
+	}
+
+	paths1 := allPaths(t.left, paths)
+	paths2 := allPaths(t.right, paths)
+
+	return append(paths1, paths2...)
+}
