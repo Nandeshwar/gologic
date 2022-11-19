6a41c40a8e8ff2dceb294efe7dcad5ff02d192fd
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-11-18 20:05:17 -0700

    updated logic

--- try2/example4_power_of_2_check/main.go
+++ try2/example4_power_of_2_check/main.go
@@ -5,23 +5,11 @@ import (
 )
 
 func main() {
-	fmt.Println("is multiple of 2......")
-	fmt.Println(isMulitplieOf2(5))
-	fmt.Println(isMulitplieOf2(10))
-	fmt.Println(isMulitplieOf2(13))
-
 	fmt.Println("is Power of 2")
 	fmt.Println(isPowerOf2(5))
 	fmt.Println(isPowerOf2(10))
 	fmt.Println(isPowerOf2(13))
 	fmt.Println(isPowerOf2(16))
-}
-
-func isMulitplieOf2(num int) bool {
-	for i := 2; i < num; {
-		i = i * 2
-	}
-	return r == num
 }
 
 func isPowerOf2(n int) bool {
