75e2f6c370eab7b05f500dfab60fb478f6b04f9a
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-12-16 15:36:40 -0700

    added a example

---
+++ try2/example138_unique_subset_all/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	a := []int{1, 2, 2}
+	// expectation: no duplicate sub arrays
+	// and in order
+	// and item should not be repeated in sub-arrays
+	// output:
+	/*
+	   [[] [1] [1 2] [1 2 2] [2] [2 2]]
+	*/
+
+	ds := []int{}
+	ans := [][]int{}
+	ind := 0
+	sort.Ints(a)
+	subset(a, ind, ds, &ans)
+	fmt.Println(ans)
+}
+
+func subset(a []int, ind int, ds []int, ans *[][]int) {
+
+	t := make([]int, len(ds))
+	copy(t, ds)
+	*ans = append(*ans, t)
+
+	for i := ind; i < len(a); i++ {
+		if i > ind && a[i] == a[i-1] {
+			continue
+		}
+
+		ds = append(ds, a[i])
+		subset(a, i+1, ds, ans)
+		ds = ds[:len(ds)-1]
+	}
+
+}
