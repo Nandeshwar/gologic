722ae7f5e3c9cb8cb40cbd019c30cd17d1a2357c
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-12-15 18:21:34 -0700

    Added example

---
+++ try2/example134_binary_number_with_alternate_bits/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 5 - 101 true : there is alternate bit
+	// 7 - 111 false
+	// 10 - 1010 : true
+
+	num := 10
+	fmt.Println(hasAlternateBits(num))
+}
+
+func hasAlternateBits(num int) bool {
+	for num != 0 {
+		prev := num % 2
+		num = num >> 1
+		curr := num % 2
+		num = num >> 1
+
+		if prev == curr {
+			return false
+		}
+	}
+	return true
+}
