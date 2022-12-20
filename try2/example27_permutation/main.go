1d1257409d3dde438ac11e70fcaa127f99eee9ce
nandeshwar.sah <nandeshwar.sah@dish.com>
2022-12-19 19:01:08 -0700

    updated example

--- try2/example27_permutation/main.go
+++ try2/example27_permutation/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	s := "abc"
 	permutations(s, 0, len(s))
+	fmt.Println("Algorithm2...")
+	var ds []string
+	visited := make([]bool, len(s))
+	permutations2(s, ds, visited)
 }
 
 func permutations(s string, left, right int) {
@@ -20,6 +25,24 @@ func permutations(s string, left, right int) {
 		s = swap(s, i, left)
 		permutations(s, left+1, right)
 		s = swap(s, i, left)
+	}
+}
+
+func permutations2(s string, ds []string, visited []bool) {
+
+	if len(ds) == len(s) {
+		fmt.Println(strings.Join(ds, ""))
+		return
+	}
+
+	for i := 0; i < len(s); i++ {
+		if !visited[i] {
+			visited[i] = true
+			ds = append(ds, string(s[i]))
+			permutations2(s, ds, visited)
+			ds = ds[:len(ds)-1]
+			visited[i] = false
+		}
 	}
 }
 
