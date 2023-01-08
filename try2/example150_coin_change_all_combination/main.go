12ddba158f37d992063e5acdfdc6ce7383a5a29b
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-01-07 17:20:24 -0700

    updated example

--- try2/example150_coin_change_all_combination/main.go
+++ try2/example150_coin_change_all_combination/main.go
@@ -18,6 +18,33 @@ func main() {
 		[3]
 		[2 1]
 	*/
+
+	fmt.Println("Algorithm2 .............")
+	result = [][]int{}
+	allCombinationArr2(coins, 0, target, []int{}, &result)
+	fmt.Println("result=", result)
+}
+
+func allCombinationArr2(a []int, ind int, target int, ds []int, result *[][]int) {
+
+	if ind == len(a) {
+
+		if target == 0 {
+			fmt.Println(ds)
+			tmp := make([]int, len(ds))
+			copy(tmp, ds)
+			*result = append(*result, tmp)
+		}
+		return
+	}
+
+	if a[ind] <= target {
+		ds = append(ds, a[ind])
+		allCombinationArr2(a, ind, target-a[ind], ds, result)
+		ds = ds[0 : len(ds)-1]
+	}
+	allCombinationArr2(a, ind+1, target, ds, result)
+
 }
 
 func allCombinationArr(coins []int, target int, ds []int, result *[][]int) {
