d13fd052db0d1a4ab10275f0659a1166faf4b8b7
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-01-13 12:58:50 -0700

    updated logic

--- try2/example106_tree_sum_of_left_leaves/main.go
+++ try2/example106_tree_sum_of_left_leaves/main.go
@@ -25,27 +25,8 @@ func main() {
 
 	t := &Node{item: 10, left: seven, right: eight}
 
-	fmt.Println(leftLeavesSum(t, 0, false))
 	fmt.Println(leftLeavesSum2(t, false))
 
-}
-
-func leftLeavesSum(t *Node, sum int, left bool) int {
-
-	if t == nil {
-		return 0
-	}
-
-	if left {
-		if t.left == nil && t.right == nil {
-			sum += t.item
-			return sum
-		}
-	}
-
-	sumFromLeft := leftLeavesSum(t.left, sum, true)
-	sumFromRight := leftLeavesSum(t.right, sum, false)
-	return sum + sumFromLeft + sumFromRight
 }
 
 func leftLeavesSum2(t *Node, left bool) int {
@@ -54,15 +35,13 @@ func leftLeavesSum2(t *Node, left bool) int {
 		return 0
 	}
 
-	sum := 0
 	if left {
 		if t.left == nil && t.right == nil {
-			sum = t.item
-			return sum
+			return t.item
 		}
 	}
 
 	sum1 := leftLeavesSum2(t.left, true)
 	sum2 := leftLeavesSum2(t.right, false)
-	return sum + sum1 + sum2
+	return sum1 + sum2
 }
