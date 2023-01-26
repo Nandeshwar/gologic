532d07c3e9564097d14c7df688779e69e09480af
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-01-26 09:52:04 -0700

    updated example

--- try2/example3_find_island/main.go
+++ try2/example3_find_island/main.go
@@ -13,6 +13,13 @@ func main() {
 		{1, 0, 0, 0},
 	}
 
+	arr2 := [][]int{
+		{1, 1, 0, 0},
+		{1, 1, 0, 0},
+		{0, 0, 1, 1},
+		{1, 0, 0, 0},
+	}
+
 	count := 0
 
 	rowLen := len(arr)
@@ -27,6 +34,16 @@ func main() {
 		}
 	}
 	fmt.Println("number of islands=", count)
+
+	maxIsland := 0
+	for i := 0; i < rowLen; i++ {
+		for j := 0; j < colLen; j++ {
+			if arr2[i][j] == 1 {
+				maxIsland = max(maxIsland, MaxIsland(arr2, rowLen, colLen, i, j))
+			}
+		}
+	}
+	fmt.Println("max islands=", maxIsland)
 }
 
 func turn1to0(arr [][]int, rowLen, colLen, i, j int) {
@@ -40,4 +57,25 @@ func turn1to0(arr [][]int, rowLen, colLen, i, j int) {
 	turn1to0(arr, rowLen, colLen, i, j+1)
 	turn1to0(arr, rowLen, colLen, i, j-1)
 
+}
+
+func MaxIsland(arr [][]int, rowLen, colLen, i, j int) int {
+	if i < 0 || i >= rowLen || j < 0 || j >= colLen || arr[i][j] == 0 {
+		return 0
+	}
+	arr[i][j] = 0
+	count := 1
+	count += MaxIsland(arr, rowLen, colLen, i+1, j)
+	count += MaxIsland(arr, rowLen, colLen, i-1, j)
+	count += MaxIsland(arr, rowLen, colLen, i, j+1)
+	count += MaxIsland(arr, rowLen, colLen, i, j-1)
+	return count
+
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
