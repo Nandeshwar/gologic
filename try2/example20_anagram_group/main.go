4c860df83f38c77e2eeb14eeb9c15a16beebc3c4
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-01-26 19:09:15 -0700

    added example

--- try2/example20_anagram_group/main.go
+++ try2/example20_anagram_group/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"unicode"
 )
 
 func main() {
 	fmt.Println(findAllAnagram([]string{"ram", "sita", "Radha", "mar", "ahdaR", "shyam"}))
+	fmt.Println(findAllAnagram2([]string{"ram", "sita", "Radha", "mar", "ahdaR", "shyam"}))
 }
 
 func findAllAnagram(strArr []string) []string {
@@ -35,4 +37,35 @@ func sortStr(s string) string {
 	sort.Strings(strArr)
 	sortedStr := strings.Join(strArr, "")
 	return sortedStr
+}
+
+func findAllAnagram2(strArr []string) []string {
+	var result []string
+	m := make(map[string][]string)
+	for _, str := range strArr {
+		a := [26]int{}
+		
+		for _, c := range str {
+			a[unicode.ToLower(c) - 'a']++
+		}
+		
+		var key string
+		for _, num := range a {
+			key += fmt.Sprintf("%d", num)
+		}
+		
+		mv, ok := m[key] 
+		if ok {
+			mv = append(mv, str)
+			m[key] = mv
+		} else {
+			mv = []string{str}
+			m[key] = mv
+		}
+	}
+	
+	for _, v := range m {
+		result = append(result, v...)
+	}
+	return result
 }
