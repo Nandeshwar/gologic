8e09ae9e31adff00f73f0d8da226d3e5f93260bd
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-02-03 16:13:50 -0700

    updated logic

--- try2/example14_1st_unique_char_index/main.go
+++ try2/example14_1st_unique_char_index/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Println(findFirstUniqueLetter("madame"))
+	fmt.Println(findFirstUniqueLetter2("madame"))
 }
 
 func findFirstUniqueLetter(str string) int {
@@ -31,4 +32,38 @@ func findFirstUniqueLetter(str string) int {
 		min = int(math.Min(float64(v), float64(min)))
 	}
 	return min
+}
+
+func findFirstUniqueLetter2(str string) int {
+	a := [26]int{}
+	for i := 0; i < 26; i++ {
+		a[i] = -1
+	}
+
+	for ind, v := range str {
+
+		if a[v-'a'] > -1 {
+			a[v-'a'] = -1
+		} else {
+			a[v-'a'] = ind
+		}
+
+	}
+	fmt.Println(a)
+
+	min := int(^uint(0) >> 1)
+	for i := 0; i < 26; i++ {
+		if a[i] == -1 {
+			continue
+		}
+		min = Min(min, a[i])
+	}
+	return min
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
