c8eb5ba835871aefc1517da8ca30c82280f35b8e
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-02-24 19:35:43 -0700

    Added a example

---
+++ try2/example191_sum_range_query_sum_of_given_indices/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := []int{-1, -2, -3, 4, 5, 6, -2}
+	// sum(0, 3) = -1 + (-2) + (-3) + 4 = -2
+	// sum(2, 4) = -3 + 4 + 5 = 6
+
+	dp := make([]int, len(a))
+	dp[0] = a[0]
+	for i := 1; i < len(a); i++ {
+		dp[i] = dp[i-1] + a[i]
+	}
+
+	fmt.Println(sumRange(0, 3, dp))
+	fmt.Println(sumRange(2, 4, dp))
+}
+
+func sumRange(i, j int, dp []int) int {
+
+	if i == 0 {
+		return dp[j]
+	}
+
+	return dp[j] - dp[i-1]
+}
