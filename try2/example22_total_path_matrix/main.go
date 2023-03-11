5d510ac183b535ae54f61c69edd85e4741b0afb7
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-03-10 18:39:19 -0700

    added example

--- try2/example22_total_path_matrix/main.go
+++ try2/example22_total_path_matrix/main.go
@@ -22,6 +22,7 @@ func main() {
 	}
 
 	fmt.Println("total path algo2=", countPath2(m-1, n-1, dp))
+	fmt.Println("total path tabulation=", countPathTabulation(m, n))
 
 	fmt.Println("total path algo3=", countPath3(m, n))
 }
@@ -45,6 +46,47 @@ func countPath2(r, c int, dp [][]int) int {
 
 	dp[r][c] = up + left
 	return up + left
+}
+
+func countPathTabulation(r, c int) int {
+	dp := make([][]int, r)
+	for i := 0; i < r; i++ {
+		dp[i] = make([]int, c)
+	}
+
+	// if r == 0 && c == 0 {
+	// 	return 1
+	// }
+
+	// if r < 0 || c < 0 {
+	// 	return 0
+	// }
+
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = 1
+			} else {
+				up := 0
+				left := 0
+
+				if i > 0 {
+					up = dp[i-1][j]
+				}
+				if j > 0 {
+					left = dp[i][j-1]
+				}
+
+				dp[i][j] = up + left
+			}
+		}
+
+	}
+
+	// up := countPath2(r-1, c, dp)
+	// left := countPath2(r, c-1, dp)
+
+	return dp[r-1][c-1]
 }
 
 func countPath3(r, c int) int {
