ae99a3b6726866322b11049d215c548e8a177ef5
nandeshwar.sah <nandeshwar.sah@dish.com>
2023-03-19 20:11:18 -0600

    updated example

--- try2/example105_shortest_path_matrix/main.go
+++ try2/example105_shortest_path_matrix/main.go
@@ -24,6 +24,12 @@ func main() {
 		{7, 8, 9},
 	}
 
+	b := [][]int{
+		{1, 2, 3},
+		{1, 1, 6},
+		{7, 8, 9},
+	}
+
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			if i-1 >= 0 && j-1 >= 0 {
@@ -37,6 +43,37 @@ func main() {
 	}
 
 	fmt.Println("minimum path=", a[2][2])
+
+	m := 3
+	n := 3
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			dp[i][j] = -1
+		}
+	}
+	fmt.Println(minPathSumInGridRec(b, dp, m-1, n-1))
+}
+
+func minPathSumInGridRec(a, dp [][]int, m, n int) int {
+	if m == 0 && n == 0 {
+		return a[m][n]
+	}
+
+	if m < 0 || n < 0 {
+		return int(^uint(0) >> 2)
+	}
+
+	if dp[m][n] != -1 {
+		return dp[m][n]
+	}
+
+	top := a[m][n] + minPathSumInGridRec(a, dp, m-1, n)
+	left := a[m][n] + minPathSumInGridRec(a, dp, m, n-1)
+
+	dp[m][n] = min(top, left)
+	return min(top, left)
 }
 
 func min(a, b int) int {
