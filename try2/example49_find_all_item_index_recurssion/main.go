04e7884b49ee6f4be794a048ae9e0317bbbfca8e
Nandeshwar Sah <nandeshwar.sah@dish.com>
2023-03-25 13:31:02 -0600

    updated logic

--- try2/example49_find_all_item_index_recurssion/main.go
+++ try2/example49_find_all_item_index_recurssion/main.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	fmt.Println(findAllIndex([]int{10, 8, 3, 5, 8, 2, 8, 1}, 0, 8))
+	fmt.Println(findAllIndex2([]int{10, 8, 3, 5, 8, 2, 8, 1}, 0, 8))
 }
 
 func findAllIndex(a []int, index, item int) []int {
@@ -13,12 +14,27 @@ func findAllIndex(a []int, index, item int) []int {
 	if index == len(a) {
 		return []int{}
 	}
-	
+
 	arr = findAllIndex(a, index+1, item)
-	
+
 	if item == a[index] {
 		arr = append(arr, index)
 		return arr
 	}
 	return arr
+}
+
+func findAllIndex2(a []int, index, item int) []int {
+	var arr []int
+	if index == len(a) {
+		return []int{}
+	}
+
+	if item == a[index] {
+		arr = append(arr, index)
+	}
+
+	arr1 := findAllIndex(a, index+1, item)
+
+	return append(arr, arr1...)
 }
