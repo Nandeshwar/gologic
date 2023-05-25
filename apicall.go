4bf187e76062b8a5ffefdf7eef732d06f126442c
Nandeshwar Sah <nandeshwar.sah@dish.com>
2023-05-24 20:32:57 -0600

    updated example

--- apicall.go
+++ apicall.go
@@ -1,3 +1,7 @@
+/*
+not related but some video for encrypt using Crypto : https://www.youtube.com/watch?v=jgTqR8PuWuU&t=663s
+*/
+
 package main
 
 import (
@@ -19,7 +23,8 @@ func main() {
 	sec2 := now2.Unix()
 
 	fmt.Println("sec=", sec2)
-	str := "nyrx4WYE+RINKbQ=="
+	//uDec, err := b64.URLEncoding.DecodeString("MIIEpAIBAAKCAQEA8mbTZwLtqy5mgA/JFmizW2zZNs62CapuqGP0Xq7SFBMp9CVI\nBWypvAkvuY4ufc1pgpEv3o5gH/e3hXHght2d0VToXsqsvOaVpIPFSmz+OjcF/mhL\n1pMXg9yoqbZMsTSWSTwolFxptJgUhSoOe+m10iGWiQ/fjarbs3cSLJRjK9PtqfIz\n4HymHbAJZY4e80ydtjgvEmLap5C/Pl/B+r0f+UCnuPAVaHLP/zFZRj1vGjSi9ldC\nge+XO0UKRYojTWXzHtLfxH108JHu6Ap2/8ej2o+9MmSGhIfAeAYiQVmDe807L1u4\n5+TqXbaimZHNxbjYCr2pHuphQ205O2cM6TlwxQIDAQABAoIBAC9hXe0Cq4YMOcjK\nTRnuOTCjpAmMehSFlb+gWgGv1ixKWqb4Ko204bB5czSnz/qTDg6RZnPwDGXzO5H/\n4k06QEDMgecVYpJB1/oiL4wOpdqnKqrBpCE6+xuxI96YXYE9sCp8ccoeDiNYnpow\n0Ef0NK/wFMaRrba5MNxxZ8IC9yd3kdg+B4ao6udYKbsRg5v2RS6HgqnJO67y7j+C\niYGoLvu42fNTUX21nQyUwgL2DOalNhVTCh+PNIggLsdl/ZybbXgvyBMo25z2lidZ\nyQrakld6xWQ9G25/PRT635DwwhHtbNwFvA+qlhcvCMXnlglqLaiCw51TyuAa6fKj\nk10AScECgYEA+oRyZ0SDdFjlmR9ofX/Tu23Rwu/VIfXr3as5KRlw2mmPx49ejAsq\nv96x1ROwmDqeF5w5uCS3ytibySl6KA1/mGmnxN9gwCNL+cWffaqYvOcYtDsde5H2\n2zZm0QZma853vw+hbva6qBucqFCyiDfHu/Ad3Y1TnmiHgbsSAuBIv6kCgYEA97To\n4b0tuNuhsFv8K2TOWrFYITUkInKZ3aYpA9UnU+l2zruCVQZTzHS3V0c2eDajR6Jb\nQ2hXu0rJF2P9p9Kk9sDk6id7TAHeQZiRGKfObwDzwpnH1CiCrNDpSeE2lF2vrbx5\nLZOBWwsfNuYFrs/tDS6PZ84IlpJbtBzmPubvCb0CgYBZ5P8ceWRmcqPo/3FCX4U9\n45l6xw8HAbUitRds3Rk29txGMvctb7Bma2YK3OboqVgjhsbbgimFm5bDZ6PTDYz0\nxy0Ro0qXh9LyjOy/bmEioBaoTfI1blpTrUDVzuMf2lXz9IrsQ5MVUds0NsjpwoJk\nuTQuVVFlLYM6lUNQTuUsCQKBgQCsyGJRY/ZKpdkI/YDOfAh8tou0zi6gYLP3Kfoe\nFEbUf1tCJQVqbXlyek+Q77mM7P/D7foe8N+RYz8Vs8exkntDK5YBxvx4Li1sMBG1\n0wdp4o4lxcLfuEo+ZZL018WMhDUQyRD1u5hVe1KQpq58G8lMkpexXsQa04hoAGiS\no99FOQKBgQDOIqRvY2nrMg48mEQx5gaIbaNkRIOKKy3IpqJAf41lSaRwO10sn6uc\nnBOYCx/+7/2OUY/MbRJCk2jX78TxWXDKB1butN7hQDAqdBYKDCUWm9O1JwK8kbRq\nW87mY2Dndj/II1OOVUvbvrD6RjAOnqQ7VlasQ24nyrx4WYE+RINKbQ==")
+	str := "MIIEpAIBAAKCAQEA8mbTZwLtqy5mgA/JFmizW2zZNs62CapuqGP0Xq7SFBMp9CVI\nBWypvAkvuY4ufc1pgpEv3o5gH/e3hXHght2d0VToXsqsvOaVpIPFSmz+OjcF/mhL\n1pMXg9yoqbZMsTSWSTwolFxptJgUhSoOe+m10iGWiQ/fjarbs3cSLJRjK9PtqfIz\n4HymHbAJZY4e80ydtjgvEmLap5C/Pl/B+r0f+UCnuPAVaHLP/zFZRj1vGjSi9ldC\nge+XO0UKRYojTWXzHtLfxH108JHu6Ap2/8ej2o+9MmSGhIfAeAYiQVmDe807L1u4\n5+TqXbaimZHNxbjYCr2pHuphQ205O2cM6TlwxQIDAQABAoIBAC9hXe0Cq4YMOcjK\nTRnuOTCjpAmMehSFlb+gWgGv1ixKWqb4Ko204bB5czSnz/qTDg6RZnPwDGXzO5H/\n4k06QEDMgecVYpJB1/oiL4wOpdqnKqrBpCE6+xuxI96YXYE9sCp8ccoeDiNYnpow\n0Ef0NK/wFMaRrba5MNxxZ8IC9yd3kdg+B4ao6udYKbsRg5v2RS6HgqnJO67y7j+C\niYGoLvu42fNTUX21nQyUwgL2DOalNhVTCh+PNIggLsdl/ZybbXgvyBMo25z2lidZ\nyQrakld6xWQ9G25/PRT635DwwhHtbNwFvA+qlhcvCMXnlglqLaiCw51TyuAa6fKj\nk10AScECgYEA+oRyZ0SDdFjlmR9ofX/Tu23Rwu/VIfXr3as5KRlw2mmPx49ejAsq\nv96x1ROwmDqeF5w5uCS3ytibySl6KA1/mGmnxN9gwCNL+cWffaqYvOcYtDsde5H2\n2zZm0QZma853vw+hbva6qBucqFCyiDfHu/Ad3Y1TnmiHgbsSAuBIv6kCgYEA97To\n4b0tuNuhsFv8K2TOWrFYITUkInKZ3aYpA9UnU+l2zruCVQZTzHS3V0c2eDajR6Jb\nQ2hXu0rJF2P9p9Kk9sDk6id7TAHeQZiRGKfObwDzwpnH1CiCrNDpSeE2lF2vrbx5\nLZOBWwsfNuYFrs/tDS6PZ84IlpJbtBzmPubvCb0CgYBZ5P8ceWRmcqPo/3FCX4U9\n45l6xw8HAbUitRds3Rk29txGMvctb7Bma2YK3OboqVgjhsbbgimFm5bDZ6PTDYz0\nxy0Ro0qXh9LyjOy/bmEioBaoTfI1blpTrUDVzuMf2lXz9IrsQ5MVUds0NsjpwoJk\nuTQuVVFlLYM6lUNQTuUsCQKBgQCsyGJRY/ZKpdkI/YDOfAh8tou0zi6gYLP3Kfoe\nFEbUf1tCJQVqbXlyek+Q77mM7P/D7foe8N+RYz8Vs8exkntDK5YBxvx4Li1sMBG1\n0wdp4o4lxcLfuEo+ZZL018WMhDUQyRD1u5hVe1KQpq58G8lMkpexXsQa04hoAGiS\no99FOQKBgQDOIqRvY2nrMg48mEQx5gaIbaNkRIOKKy3IpqJAf41lSaRwO10sn6uc\nnBOYCx/+7/2OUY/MbRJCk2jX78TxWXDKB1butN7hQDAqdBYKDCUWm9O1JwK8kbRq\nW87mY2Dndj/II1OOVUvbvrD6RjAOnqQ7VlasQ24nyrx4WYE+RINKbQ=="
 
 	sDec, err2 := b64.StdEncoding.DecodeString(str)
 	if err2 != nil {
@@ -32,7 +37,7 @@ func main() {
 	sec := now.Unix()
 
 	beginDate := "2023-06-05"
-	url := fmt.Sprintf("my url", beginDate, sec)
+	url := fmt.Sprintf("https://cfeeds-content-publisher-api.content-feed.clusters.pluto.tv/content/aero/v1/epg/5dcc42446750e200093b15e2/%s?page=1&results_per_page=100&client_id=slingtv&sling_tv_timestamp=%d", beginDate, sec)
 	fmt.Println("url=", url)
 
 	//block, input := pem.Decode([]byte(str))
