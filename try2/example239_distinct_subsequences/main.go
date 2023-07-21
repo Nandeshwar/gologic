4ba9a8e4b636c922814229909522e5063386c16d
Nandeshwar Sah <nandeshwar.sah@dish.com>
2023-07-21 11:09:21 -0600

    updated example

--- try2/example239_distinct_subsequences/main.go
+++ try2/example239_distinct_subsequences/main.go
@@ -26,6 +26,14 @@ func main() {
 		}
 	}
 	fmt.Println(distinctSubsequence(s1, s2, i, j, dp))
+
+	fmt.Println("Algorithm2...")
+	for i := 0; i < len(dp); i++ {
+		for j := 0; j < len(s2); j++ {
+			dp[i][j] = -1
+		}
+	}
+	fmt.Println(distinctSubsequence2(s1, s2, 0, 0, dp))
 }
 
 func distinctSubsequence(s1, s2 string, i, j int, dp [][]int) int {
@@ -50,6 +58,32 @@ func distinctSubsequence(s1, s2 string, i, j int, dp [][]int) int {
 	}
 
 	cnt := distinctSubsequence(s1, s2, i-1, j, dp) // i-1, j : fix with s2 and try to match with all characters of s1
+	dp[i][j] = cnt
+	return cnt
+}
+
+func distinctSubsequence2(s1, s2 string, i, j int, dp [][]int) int {
+	if j >= len(s2) {
+		return 1 // j < 0 that means found
+	}
+
+	if i >= len(s1) {
+		return 0
+	}
+
+	if dp[i][j] != -1 {
+		fmt.Println("dynamic programming working")
+		return dp[i][j]
+	}
+
+	if s1[i] == s2[j] {
+
+		cnt := distinctSubsequence2(s1, s2, i+1, j+1, dp) + distinctSubsequence2(s1, s2, i+1, j, dp) // i+1, j : fix substring and check other character in 1st string
+		dp[i][j] = cnt
+		return cnt
+	}
+
+	cnt := distinctSubsequence2(s1, s2, i+1, j, dp) // i+1, j : fix with s2 and try to match with all characters of s1
 	dp[i][j] = cnt
 	return cnt
 }
