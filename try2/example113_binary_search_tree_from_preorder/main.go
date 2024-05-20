4ccb6ddbeafe66c9264a48a480db520f3dda84b5
Nandeshwar Sah <nandeshwar.sah@dish.com>
2024-05-20 13:59:34 -0600

    updated example

--- try2/example113_binary_search_tree_from_preorder/main.go
+++ try2/example113_binary_search_tree_from_preorder/main.go
@@ -36,6 +36,37 @@ func main() {
 	displayTree(t)
 }
 
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func bstFromPreorder(preorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	if len(preorder) == 1 {
+		return &TreeNode{Val: preorder[0]}
+	}
+
+	root := &TreeNode{}
+	root.Val = preorder[0]
+
+	i := 1
+	for i < len(preorder) && preorder[i] < preorder[0] {
+		i++
+	}
+
+	root.Left = bstFromPreorder(preorder[1:i])
+	root.Right = bstFromPreorder(preorder[i:])
+
+	return root
+
+}
+
 func bbt(a []int) *Tree {
 	if len(a) == 0 {
 		return nil
@@ -48,7 +79,7 @@ func bbt(a []int) *Tree {
 
 	var leftIndex int
 	var rightIndex int
-	
+
 	for _, item := range a[1:] {
 		if item < a[0] {
 			leftIndex++
