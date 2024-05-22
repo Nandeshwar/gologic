a20db2d4bc03e9f4b8534d54e8a74886b502743c
Nandeshwar Sah <nandeshwar.sah@dish.com>
2024-05-22 09:54:59 -0600

    added example

--- try2/example171_word_search_two_dimensional_array/main.go
+++ try2/example171_word_search_two_dimensional_array/main.go
@@ -11,12 +11,33 @@ func main() {
 		{'m', 'o', 'p'},
 	}
 
-	word := "ram"
+	b := [][]byte{
+		{'a', 'm', 'm'},
+		{'k', 'a', 'r'},
+		{'m', 'o', 'p'},
+	}
 
+	word := "ram"
+out:
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
 			if a[i][j] == rune(word[0]) {
 				if search(a, word, i, j, 0) {
+
+					fmt.Println("true")
+					break out
+				} else {
+					fmt.Println("false")
+					break out
+				}
+			}
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if a[i][j] == rune(word[0]) {
+				if checkInAllDirections(b, word, i, j, 0) {
 
 					fmt.Println("true")
 					return
@@ -27,6 +48,25 @@ func main() {
 			}
 		}
 	}
+}
+func checkInAllDirections(board [][]byte, word string, i, j int, count int) bool {
+
+	if count == len(word) {
+		return true
+	}
+	if i < 0 || i >= len(board) || j < 0 || j >= len(board[i]) || board[i][j] != word[count] {
+		return false
+	}
+
+	tmp := board[i][j]
+	board[i][j] = ' '
+	count1 := checkInAllDirections(board, word, i+1, j, count+1)
+	count2 := checkInAllDirections(board, word, i-1, j, count+1)
+	count3 := checkInAllDirections(board, word, i, j+1, count+1)
+	count4 := checkInAllDirections(board, word, i, j-1, count+1)
+	board[i][j] = tmp
+
+	return count1 || count2 || count3 || count4
 }
 
 func search(a [][]rune, word string, row, col int, count int) bool {
@@ -39,11 +79,13 @@ func search(a [][]rune, word string, row, col int, count int) bool {
 		return false
 	}
 
-	//tmp := a[row][col]
-	return search(a, word, row+1, col, count+1) ||
+	tmp := a[row][col]
+	a[row][col] = ' '
+	found := search(a, word, row+1, col, count+1) ||
 		search(a, word, row-1, col, count+1) ||
 		search(a, word, row, col+1, count+1) ||
 		search(a, word, row, col-1, count+1)
-	//a[row][col] = tmp
+	a[row][col] = tmp
+	return found
 
 }
